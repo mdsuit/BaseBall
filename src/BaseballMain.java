d2fa1df24ff5e834cf5898b64bda90c4828db9f1
Suit, Michael D <MICHAEL_D_SUIT@homedepot.com>
2017-06-28 11:06:20 -0400

    Merged code

--- src/BaseballMain.java
+++ src/BaseballMain.java
@@ -1,7 +1,38 @@
+import javax.swing.plaf.nimbus.State;
+import java.util.ArrayList;
+
 /**
  * Created by MDS1728 on 6/1/2017.
  */
 public class BaseballMain {
 
+    public static void main(String[] args) {
+      //  String[][] numbers = new String[9][9];
+        ArrayList players;
+        players = new ArrayList();
+        players.size();
+        players.add("Player 1");
+        players.add("Player 2");
+        players.add("Player 3");
+        players.add("Player 4");
+        players.add("Player 5");
+        players.add("Player 6");
+        players.add("Player 7");
+        players.add("Player 8");
+        players.add("Player 9");
+        System.out.println();
 
-}
+
+        /*
+        for (int row = 0; row < players.length; row++) {
+            for (int col = 0; col < players[row].length; col++) {
+                players[row][col] = String.valueOf(row) + String.valueOf(col);
+                System.out.print(players[row][col] + " ");
+            }
+            System.out.println(" ");
+            */
+        }
+
+    }
+
+
--- src/Utilities.java
+++ src/Utilities.java
@@ -3,25 +3,42 @@
  */
 public class Utilities {
     public static void main(String[] args) {
-
     }
-
     //extends BaseballGame class
 
     //constructor
 
-    //Random number generator
+    {
 
-     {
-        //  determines outs and bases from random number
-     }
+    }
+    //  determines outs and bases from random number
+
+    public class atBat {
+        public void main(String[] args) {
+            atBat myatBat = new atBat();
+            int score = myatBat.randomNumber(100);
+            if (score <= 70)
+                System.out.println("Out");
+            else if (score >= 71 && score <= 87)
+                System.out.println("Single");
+            else if (score >= 88 && score <= 94)
+                System.out.println("Double");
+            else if (score >= 95 && score <= 96)
+                System.out.println("Tripple");
+            else if (score >= 97 && score <= 100)
+                System.out.println("Home Run");
+        }
+
+        //Generates random number
         public int randomNumber(int limit) {
-        int score;
-        score = ((int) (Math.random() * limit)) + 1;
-        // System.out.println(score);
-        return score;
+            int score;
+            score = ((int) (Math.random() * limit)) + 1;
+            //Remove comment to show random number
+            System.out.println(score);
+            return score;
 
 
-        //Prints Array, Double Array or array list}
+            //Prints Array, Double Array or array list}
+        }
     }
 }
